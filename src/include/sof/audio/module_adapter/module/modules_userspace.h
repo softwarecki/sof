4f290b148d03a2a9dab3ce79fabd4768b9d0f7f0
Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
2023-09-15 14:49:23 +0200

    [userspace] ptl: mmu: Introduce userspace worker to service IPC [INTERNAL]
    The non-privileged modules code should be executed in separate thread
    executed in userspace mode. This way the code and data owned by such
    module will be isolated from other non-privileged modules in the system.
    
    The implementation creates user_work_queue threads that will service all
    IPC's for all such modules. There are security domains defined that will
    combines service of IPC's for modules that could exist without isolation
    (eg. several instances of the same module or modules originating from single
    vendor).
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
    
    ptl: mmu: Fix memory partition initialization for modules [INTERNAL]
    
    Non-privileged modules need access to shared memory pool space in
    cached and non-cached regions to get access to common base FW
    objects.
    There was mistake in previous implementation, that only non-chached region
    was added to domain and module's stack in cached region was used.
    This patch fixes that mistake. Module stack is accessible thorough Zephyr
    internal mechanisms.
    NOTICE: Subject to squash after merge.
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
    
    ptl: mmu: Add support for static semaphores for modules [INTERNAL]
    
    There is Zephyr limitation that only statically allocated k_objects like
    semaphores and mutex could be used in non-privileged modules.
    This patch creates table of MODULE_MAX_NUMBER of semaphores and
    provides mechanism to dynamic assignment of semaphore for given thread.
    This could be removed in future when semaphores will be added to
    dynamic k_objects supported in userspace.userspace_helper
    
    Signed-off-by: Jaroslaw Stelter <Jaroslaw.Stelter@intel.com>
    
    userspace
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

---
+++ src/include/sof/audio/module_adapter/module/modules_userspace.h
@@ -0,0 +1,70 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright(c) 2016 Intel Corporation. All rights reserved.
+ *
+ * Author: Jaroslaw Stelter <jaroslaw.stelter@intel.com>
+ * Author: Adrian Warecki <adrian.warecki@intel.com>
+ */
+
+#ifndef __SOF_AUDIO_MODULES_USERSPACE_H__
+#define __SOF_AUDIO_MODULES_USERSPACE_H__
+
+#if CONFIG_USERSPACE
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/app_memory/app_memdomain.h>
+#include <utilities/array.h>
+
+#define APP_USER_DATA	K_APP_DMEM(ipc_partition)
+#define APP_USER_BSS	K_APP_BMEM(ipc_partition)
+
+#include <module/module/interface.h>
+
+struct module_interface;
+struct comp_driver;
+struct sof_man_module;
+
+/* Processing module structure fields needed for user mode */
+struct userspace_module {
+	struct k_mem_domain *comp_dom;			/* Module specific memory domain	*/
+	uint32_t security_domain_id;			/* Determine security domain id number	*/
+	void *wrk_ctx;					/* Work queue context			*/
+	const struct module_interface *interface;	/* Userspace module interface		*/
+};
+
+typedef int (*system_agent_start_fn)(uintptr_t entry_point, uint32_t module_id,
+				     uint32_t instance_id, uint32_t core_id, uint32_t log_handle,
+				     void *mod_cfg, void **adapter);
+
+/**
+ * Creates userspace module adapter
+ *
+ * @param user_mod - pointer to pointer of userspace module data
+ * @param drv - pointer to component driver
+ * @param manifest - pointer to module manifest
+ * @param agent - pointer to system agent start function
+ * @param entry_point - loadable module entry point
+ * @param module_id - module id
+ * @param instance_id - instance id
+ * @param core_id - core id - unused
+ * @param log_handle - log handle
+ * @param mod_cfg - ipc4 module configuration
+ * @param interface - pointer to variable to store created module interface
+ * @return 0 for success, error otherwise.
+ */
+int userspace_module_create(struct userspace_module **user_mod, const struct comp_driver *drv,
+			    const struct sof_man_module *manifest, system_agent_start_fn start_fn,
+			    uintptr_t entry_point, uint32_t module_id, uint32_t instance_id,
+			    uint32_t core_id, uint32_t log_handle, byte_array_t *mod_cfg,
+			    void **adapter);
+
+void userspace_module_destroy(const struct comp_driver *drv, struct userspace_module *mod);
+
+#else
+
+#define APP_USER_DATA
+
+#endif /* CONFIG_USERSPACE */
+#endif /* __SOF_AUDIO_MODULES_USERSPACE_H__ */
