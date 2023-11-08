833e082f23c20438d92e59712069a4eb57fb78de
Adrian Warecki <adrian.warecki@intel.com>
2023-11-08 15:15:02 +0100

    api version check

--- src/include/module/module/api_ver.h
+++ src/include/module/module/api_ver.h
@@ -10,15 +10,15 @@
 
 #include <stdint.h>
 
-#define ADSP_BUILD_INFO_FORMAT 0
+#define SOF_MODULE_API_BUILD_INFO_FORMAT	0x80000000
 
 /*
  *  Api version 5.0.0 for sof loadable modules
  */
 
-#define SOF_MODULE_API_MAJOR_VERSION 5
-#define SOF_MODULE_API_MIDDLE_VERSION 0
-#define SOF_MODULE_API_MINOR_VERSION 0
+#define SOF_MODULE_API_MAJOR_VERSION	5
+#define SOF_MODULE_API_MIDDLE_VERSION	0
+#define SOF_MODULE_API_MINOR_VERSION	0
 
 union sof_module_api_version {
 	uint32_t full;
@@ -30,9 +30,19 @@ union sof_module_api_version {
 	} fields;
 };
 
-struct sof_module_api_build_info{
+struct sof_module_api_build_info {
 	uint32_t format;
 	union sof_module_api_version api_version_number;
 };
+
+#define DECLARE_LOADABLE_MODULE_API_VERSION(name)						\
+struct sof_module_api_build_info name ## _build_info __attribute__((section(".buildinfo"))) = {	\
+	SOF_MODULE_API_BUILD_INFO_FORMAT,							\
+	{											\
+		((0x3FF & SOF_MODULE_API_MAJOR_VERSION)  << 20) |				\
+		((0x3FF & SOF_MODULE_API_MIDDLE_VERSION) << 10) |				\
+		((0x3FF & SOF_MODULE_API_MINOR_VERSION)  << 0)					\
+	}											\
+}
 
 #endif /* __MODULE_MODULE_API_VER_H__ */
