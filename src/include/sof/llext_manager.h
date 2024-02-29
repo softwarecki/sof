828dcc25f23bb4d99ba184fa0eb8c32da6debfb9
Adrian Warecki <adrian.warecki@intel.com>
2024-02-29 16:46:13 +0100

    lib_manager: llext_manager: Add const to module allocate functions
    Added a const modifier to the ipc_config parameter passed to functions
    lib_manager_allocate_module and llext_manager_allocate_module.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/include/sof/llext_manager.h
+++ src/include/sof/llext_manager.h
@@ -12,7 +12,7 @@ struct comp_driver;
 struct comp_ipc_config;
 
 uint32_t llext_manager_allocate_module(const struct comp_driver *drv,
-				       struct comp_ipc_config *ipc_config,
+				       const struct comp_ipc_config *ipc_config,
 				       const void *ipc_specific_config);
 int llext_manager_free_module(const uint32_t component_id);
 
