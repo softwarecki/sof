1412e353e9e7e014b813e885cb8793b19bafb9f9
Adrian Warecki <adrian.warecki@intel.com>
2024-02-29 13:11:35 +0100

    modules: Remove unused module_entry_point from module_data
    The value assigned to the module_entry_point field in the module_data
    structure wasn't used anywhere. This field has been removed.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/include/module/module/base.h
+++ src/include/module/module/base.h
@@ -58,7 +58,6 @@ struct module_data {
 	void *runtime_params;
 	struct module_memory memory; /**< memory allocated by module */
 	struct module_processing_data mpd; /**< shared data comp <-> module */
-	uint32_t module_entry_point; /**<loadable module entry point address */
 #endif /* SOF_MODULE_PRIVATE */
 };
 
