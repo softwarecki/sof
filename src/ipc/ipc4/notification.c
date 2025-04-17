0ad501dffcaf0acb2db53ba24ec0345eb2771344
Adrian Warecki <adrian.warecki@intel.com>
2025-04-17 13:54:34 +0200

    ipc4: notification: Add support for data process error notification
    Provide a structure and function to initiate notification of an error
    reported by the module's data processing function.
    
    Signed-off-by: Adrian Warecki <adrian.warecki@intel.com>

--- src/ipc/ipc4/notification.c
+++ src/ipc/ipc4/notification.c
@@ -33,3 +33,15 @@ void xrun_notif_msg_init(struct ipc_msg *msg_xrun, uint32_t resource_id, uint32_
 	notif_data->resource_type = SOF_IPC4_GATEWAY;
 }
 #endif
+
+void process_data_error_notif_msg_init(struct ipc_msg *msg, uint32_t resource_id,
+				       uint32_t error_code)
+{
+	struct ipc4_resource_event_data_notification *notif_data = msg->tx_data;
+	resource_notif_header_init(msg);
+
+	notif_data->resource_id = resource_id;
+	notif_data->event_type = SOF_IPC4_PROCESS_DATA_ERROR;
+	notif_data->resource_type = SOF_IPC4_MODULE_INSTANCE;
+	notif_data->event_data.process_data_error.error_code = error_code;
+}
